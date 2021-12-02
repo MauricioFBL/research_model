9ba1f5feed6e5c4912ef5e0ca2715db0bb6407de
Mauricio Bautista <Mauricio.Bautista@dentsu.com>
2021-12-02 11:26:01 -0600

    Se agrego la url de pagina

--- remote_work.py
+++ remote_work.py
@@ -1,54 +1,61 @@
 import urllib.request as urllib2
 import pandas as pd
-from bs4 import BeautifulSoup, SoupStrainer
+import datetime as dt
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
 
-url = "https://remoteok.com"
-opener = urllib2.build_opener()
-opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
-website = opener.open(url)
+class remoteok_scrapper():
+    url = "https://remoteok.com"
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
+    website = opener.open(url)
+    today = (str)(dt.date.today())
 
-html = website.read()
-# print(html)
-soup = BeautifulSoup(html, "html.parser")
-position = []
-enterprise = []
-sallarie = []
-publish = []
-# position = []
-for h in soup.find_all('h2', {"itemprop": "title"}):
-    # print(h.get('h3').text)
-    position.append(str.strip(h.text))
-    # print(str.strip(h.text))
+    html = website.read()
+    # print(html)
+    soup = BeautifulSoup(html, "html.parser")
+    position = []
+    enterprise = []
+    sallarie = []
+    publish = []
+    # position = []
+    for h in soup.find_all('h2', {"itemprop": "title"}):
+        # print(h.get('h3').text)
+        position.append(str.strip(h.text))
+        # print(str.strip(h.text))
 
-for h in soup.find_all('h3', {"itemprop": "name"}):
-    # print(h.get('h3').text)
-    enterprise.append(str.strip(h.text))
-    # print(str.strip(h.text))
+    for h in soup.find_all('h3', {"itemprop": "name"}):
+        # print(h.get('h3').text)
+        enterprise.append(str.strip(h.text))
+        # print(str.strip(h.text))
 
-for h in soup.find_all("div", {"class": "location"}):
-    sallarie.append(str.strip(h.text))
-    # print(str.strip(h.text))
+    for h in soup.find_all("div", {"class": "location"}):
+        sallarie.append(str.strip(h.text))
+        # print(str.strip(h.text))
 
-for i in soup.findAll('time'):
-        if i.has_attr('datetime'):
-            publish.append(i['datetime'])
-            print(i['datetime'])
+    for i in soup.findAll('time'):
+            if i.has_attr('datetime'):
+                publish.append(i['datetime'])
+                print(i['datetime'])
 
-# print(sallarie)
-sallary = [element for element in sallarie if element.startswith('💰 ')]
-location = [element for element in sallarie if not element.startswith('💰 ')]
+    for a in soup.find_all('a', href=True):
+        print("Found the URL:", a['href'])
 
-df = pd.DataFrame(list(zip(position, enterprise,sallary,location,publish)),
-               columns =['Posicion', 'Empresa','Salario','Ubicacion','Fecha de publicacion'])
-# print(df.head(5))
-print(df)
-df['Sitio'] = 'https://remoteok.com'
+    # print(sallarie)
+    sallary = [element for element in sallarie if element.startswith('💰 ')]
+    location = [element for element in sallarie if not element.startswith('💰 ')]
 
-print(df)
-df.to_csv('example.csv', encoding='utf-8-sig',index=False)
+    df = pd.DataFrame(list(zip(position, enterprise,sallary,location,publish)),
+                columns =['Posicion', 'Empresa','Salario','Ubicacion','Fecha de publicacion'])
+    # print(df.head(5))
+    print(df)
+    df['Sitio'] = 'https://remoteok.com'
+
+    print(df)
+    df.to_csv(F'REMOTEOK_{today}_OFFERS.csv', encoding='utf-8-sig',index=False)
 
 
 
-# for element in soup.find_all('h3'):
-#     link = element.get('text')
-#     print(link)
+    # for element in soup.find_all('h3'):
+    #     link = element.get('text')
+    #     print(link)
