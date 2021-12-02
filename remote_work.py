d65fc0e7b2153fe45149eac8fb8335caad447e1d
Mauricio Bautista <Mauricio.Bautista@dentsu.com>
2021-12-02 11:53:50 -0600

    Se agrego modulo obtener urls de vacante

--- remote_work.py
+++ remote_work.py
@@ -18,38 +18,34 @@ class remoteok_scrapper():
     enterprise = []
     sallarie = []
     publish = []
-    # position = []
+    position_url = []
+    
     for h in soup.find_all('h2', {"itemprop": "title"}):
-        # print(h.get('h3').text)
         position.append(str.strip(h.text))
-        # print(str.strip(h.text))
-
+        
     for h in soup.find_all('h3', {"itemprop": "name"}):
-        # print(h.get('h3').text)
         enterprise.append(str.strip(h.text))
-        # print(str.strip(h.text))
-
+        
     for h in soup.find_all("div", {"class": "location"}):
         sallarie.append(str.strip(h.text))
-        # print(str.strip(h.text))
-
+        
     for i in soup.findAll('time'):
             if i.has_attr('datetime'):
                 publish.append(i['datetime'])
-                print(i['datetime'])
 
-    for a in soup.find_all('a', href=True):
-        print("Found the URL:", a['href'])
+    for a in soup.find_all("a",{"rel": "noindex nofollow"}):
+        position_url.append("https://remoteok.com/"+a['href'])
 
-    # print(sallarie)
+
     sallary = [element for element in sallarie if element.startswith('💰 ')]
     location = [element for element in sallarie if not element.startswith('💰 ')]
 
     df = pd.DataFrame(list(zip(position, enterprise,sallary,location,publish)),
                 columns =['Posicion', 'Empresa','Salario','Ubicacion','Fecha de publicacion'])
     # print(df.head(5))
-    print(df)
-    df['Sitio'] = 'https://remoteok.com'
+
+    df['url Sitio'] = 'https://remoteok.com'
+    df['Sitio'] = 'REMOTEOK'
 
     print(df)
     df.to_csv(F'REMOTEOK_{today}_OFFERS.csv', encoding='utf-8-sig',index=False)
