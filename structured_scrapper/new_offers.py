0cdb42783e7a1be3bc6a7de7131ac00ff37f1c82
Mauricio Bautista <Mauricio.Bautista@dentsu.com>
2021-12-04 10:55:43 -0600

    se cambio el metodo fin all por señlect para uso de queriess

--- structured_scrapper/new_offers.py
+++ structured_scrapper/new_offers.py
@@ -13,7 +13,7 @@ class NewOffer:
         self.soup = self._get_html(self.url)
         self._get_data(self.soup,self._queries)
 
-    def _get_html(sel, url):
+    def _get_html(self, url):
         opener = urllib2.build_opener()
         opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
         website = opener.open(url)
@@ -22,12 +22,26 @@ class NewOffer:
         return soup
 
     def _get_data(self, soup, queries):
+        print(queries)
         position = [str.strip(p.text) for p in soup.select(queries['position_name'])]
         enterprise = [str.strip(p.text) for p in soup.select(queries['enterprise_name'])]
-        location = [str.strip(p.text) for p in soup.find_all("div", {"class": "location"}) if not (str.strip(p.text)).startswith('💰 ')]
-        sallary = [str.strip(p.text) for p in soup.find_all("div", {"class": "location"}) if (str.strip(p.text)).startswith('💰 ')]
+        # location = [str.strip(p.text) for p in soup.find_all("div", {"class": "location"}) if not (str.strip(p.text)).startswith('💰 ')]
+        # sallary = [str.strip(p.text) for p in soup.find_all("div", {"class": "location"}) if (str.strip(p.text)).startswith('💰 ')]
+        location = [str.strip(p.text) for p in soup.select(queries['location_salary']) if not (str.strip(p.text)).startswith('💰 ')]
+        sallary = [str.strip(p.text) for p in soup.select(queries['location_salary']) if (str.strip(p.text)).startswith('💰 ')]
         publish = [i['datetime'] for i in soup.select(queries['publish_date']) if i.has_attr('datetime')]
         position_url = [("https://remoteok.com/"+a['href']) for a in soup.find_all("a",{"rel": "noindex nofollow"})]
+
+        for h in soup.find_all("div",{'class':'description'}):
+            a =''
+            # print('---------------------------------------------')
+            for x in h.find_all('div',{'class':'markdown'}):
+                a = a + x.text
+                # print(a)
+
+
+
+
         df = pd.DataFrame(list(zip(position, enterprise,sallary,location,publish,position_url)),
             columns =['Posicion', 'Empresa','Salario','Ubicacion','Fecha de publicacion', 'URL de la vacante'])
         df['Home URL'] = 'https://remoteok.com'
@@ -36,7 +50,15 @@ class NewOffer:
         today = dt.date.today()
         df.to_csv(f'REMOTEOK_{today}_offers.csv',encoding='utf-8-sig', index=False)
     
-        # print(position_url)
+
+
+    def get_description(self,urls):
+        descriptons = []
+        for url in urls:
+            soup = self._get_html(url)
+            
+
+
 
 if __name__=='__main__':
     a = NewOffer()
