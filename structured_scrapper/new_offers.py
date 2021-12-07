1b435c12b781322aed70383f894b748468acdb67
Mauricio Bautista <Mauricio.Bautista@dentsu.com>
2021-12-07 16:02:33 -0600

    se eliminaros los mensajes de preuebas

--- structured_scrapper/new_offers.py
+++ structured_scrapper/new_offers.py
@@ -21,39 +21,30 @@ class NewOffer:
         soup = BeautifulSoup(html, "html.parser")
         return soup
 
+    def get_description(self,urls):
+        descriptions = []
+        for url in urls:
+            soup = self._get_html(url)
+            description = [str.strip(p.text) for p in soup.select(self._queries['description'])]
+            descriptions.append(description)
+        
+        return descriptions
+
     def _get_data(self, soup, queries):
         position = [str.strip(p.text) for p in soup.select(queries['position_name'])]
         enterprise = [str.strip(p.text) for p in soup.select(queries['enterprise_name'])]
-        # location = [str.strip(p.text) for p in soup.find_all("div", {"class": "location"}) if not (str.strip(p.text)).startswith('💰 ')]
-        # sallary = [str.strip(p.text) for p in soup.find_all("div", {"class": "location"}) if (str.strip(p.text)).startswith('💰 ')]
         location = [str.strip(p.text) for p in soup.select(queries['location_salary']) if not (str.strip(p.text)).startswith('💰 ')]
         sallary = [str.strip(p.text) for p in soup.select(queries['location_salary']) if (str.strip(p.text)).startswith('💰 ')]
         publish = [i['datetime'] for i in soup.select(queries['publish_date']) if i.has_attr('datetime')]
-        # position_url = [("https://remoteok.com/"+a['href']) for a in soup.find_all("a",{"rel": "noindex nofollow"})]
-        position_url = [("https://remoteok.com/"+a['href']) for a in soup.select(queries['offer_url'])]
-
-        for h in soup.find_all("div",{'class':'description'}):
-            a =''
-            # print('---------------------------------------------')
-            for x in h.find_all('div',{'class':'markdown'}):
-                a = a + x.text
-                # print(a)
-
-        df = pd.DataFrame(list(zip(position, enterprise,sallary,location,publish,position_url)),
-            columns =['Posicion', 'Empresa','Salario','Ubicacion','Fecha de publicacion', 'URL de la vacante'])
+        position_url = ['https://remoteok.com'+h['href'] for h in soup.select(queries['offer_url'])]
+        description = self.get_description(position_url)
+        df = pd.DataFrame(list(zip(position, enterprise,sallary,location,publish,position_url,description)),
+            columns =['Posicion', 'Empresa','Salario','Ubicacion','Fecha de publicacion', 'URL de la vacante', 'Descripción'])
         df['Home URL'] = 'https://remoteok.com'
         df['Nombre del Sitio'] = 'REMOTEOK'
         print(df)
         today = dt.date.today()
         df.to_csv(f'REMOTEOK_{today}_offers.csv',encoding='utf-8-sig', index=False)
-    
-
-
-    def get_description(self,urls):
-        descriptons = []
-        for url in urls:
-            soup = self._get_html(url)
-            
 
 
 
